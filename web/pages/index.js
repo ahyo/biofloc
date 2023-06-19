fecf6b1d8c26d56cc720269f84f334d19f115288
Ahyo Haryanto <ahyo.haryanto@gmail.com>
2023-06-19 12:42:39 +0700

    update

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,10 +1,20 @@
-import GuestLayout from "../components/GuestLayout";
+import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
+import { Spinner } from "react-bootstrap";
+
 
 function Home() {
+  const router = useRouter();
+  const token = useSelector(state => state.auth.token);
+  if (token){
+    router.push('/dashboard')
+  }else{
+    router.push('/login')
+  }
   return (
-    <GuestLayout>
-      <h1>Welcome</h1>
-    </GuestLayout>
+    <div>
+      <Spinner animation="grow" variant="warning" />
+    </div>
   );
 }
 
